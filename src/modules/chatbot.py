cd220309f54351c645078727ef4e99c74dbab813
Abenezer Nuro <anuro@artba.org>
2023-05-10 10:52:34 -0400

    dd

---
+++ src/modules/chatbot.py
@@ -0,0 +1,50 @@
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.prompts.prompt import PromptTemplate
+from langchain.callbacks import get_openai_callback
+
+class Chatbot:
+
+    def __init__(self, model_name, temperature, vectors):
+        self.model_name = model_name
+        self.temperature = temperature
+        self.vectors = vectors
+
+    qa_template = """You are a friendly conversational assistant named Robby, designed to answer questions and chat with the user from a contextual file.
+        You receive data from a user's file and a question, you must help the user find the information they need. 
+        Your answers must be user-friendly and respond to the user in the language they speak to you.
+        question: {question}
+        =========
+        context: {context}
+        ======="""
+    
+    QA_PROMPT = PromptTemplate(template=qa_template, input_variables=["question", "context"])
+
+    def conversational_chat(self, query):
+        """
+        Start a conversational chat with a model via Langchain
+        """
+        llm = ChatOpenAI(model_name=self.model_name, temperature=self.temperature)
+
+        retriever = self.vectors.as_retriever()
+
+        chain = ConversationalRetrievalChain.from_llm(llm=llm,
+            retriever=retriever, verbose=True, return_source_documents=True)
+
+        chain_input = {"question": query, "chat_history": st.session_state["history"]}
+        result = chain(chain_input)
+
+        st.session_state["history"].append((query, result["answer"]))
+        #count_tokens_chain(chain, chain_input)
+        return result["answer"]
+
+
+def count_tokens_chain(chain, query):
+    with get_openai_callback() as cb:
+        result = chain.run(query)
+        st.write(f'###### Tokens used in this conversation : {cb.total_tokens} tokens')
+    return result 
+
+    
+    
